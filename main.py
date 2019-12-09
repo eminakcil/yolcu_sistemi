3f9a9d6f20135e01ad5cdaad6b07daa0de5828cf
eminakcil <eminakcil@outlook.com>
2019-12-09 16:38:16 +0300

    37 koltuk etkinlestirildi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from functools import partial
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, pyqtSlot
 
 from views.yolcuEkle import Ui_MainWindow
 from controllers.slots import MainController
@@ -20,18 +20,19 @@ class MyWindow(QtWidgets.QMainWindow):
 
         # temel degiskenler
         self.koltuklar = [
-            self.ui.koltuk_1_btn,
-            self.ui.koltuk_2_btn,
-            self.ui.koltuk_3_btn,
-            self.ui.koltuk_4_btn,
-            self.ui.koltuk_5_btn,
-            self.ui.koltuk_6_btn,
-            self.ui.koltuk_7_btn,
-            self.ui.koltuk_8_btn,
-            self.ui.koltuk_9_btn,
-            self.ui.koltuk_10_btn,
-            self.ui.koltuk_11_btn,
-            self.ui.koltuk_12_btn
+            self.ui.koltuk_1_btn, self.ui.koltuk_2_btn, self.ui.koltuk_3_btn,
+            self.ui.koltuk_4_btn, self.ui.koltuk_5_btn, self.ui.koltuk_6_btn,
+            self.ui.koltuk_7_btn, self.ui.koltuk_8_btn, self.ui.koltuk_9_btn,
+            self.ui.koltuk_10_btn, self.ui.koltuk_11_btn, self.ui.koltuk_12_btn,
+            self.ui.koltuk_13_btn, self.ui.koltuk_14_btn,  self.ui.koltuk_15_btn,
+            self.ui.koltuk_16_btn, self.ui.koltuk_17_btn, self.ui.koltuk_18_btn,
+            self.ui.koltuk_19_btn, self.ui.koltuk_20_btn, self.ui.koltuk_21_btn,
+            self.ui.koltuk_22_btn, self.ui.koltuk_23_btn, self.ui.koltuk_24_btn,
+            self.ui.koltuk_25_btn, self.ui.koltuk_26_btn, self.ui.koltuk_27_btn,
+            self.ui.koltuk_28_btn, self.ui.koltuk_29_btn, self.ui.koltuk_30_btn,
+            self.ui.koltuk_31_btn, self.ui.koltuk_32_btn, self.ui.koltuk_33_btn,
+            self.ui.koltuk_34_btn, self.ui.koltuk_35_btn, self.ui.koltuk_36_btn,
+            self.ui.koltuk_37_btn
         ]
 
         self.koltuk = ""
@@ -76,6 +77,31 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.koltuk_10_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_10_btn))
         self.ui.koltuk_11_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_11_btn))
         self.ui.koltuk_12_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_12_btn))
+        self.ui.koltuk_13_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_13_btn))
+        self.ui.koltuk_14_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_14_btn))
+        self.ui.koltuk_15_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_15_btn))
+        self.ui.koltuk_16_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_16_btn))
+        self.ui.koltuk_17_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_17_btn))
+        self.ui.koltuk_18_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_18_btn))
+        self.ui.koltuk_19_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_19_btn))
+        self.ui.koltuk_20_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_20_btn))
+        self.ui.koltuk_21_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_21_btn))
+        self.ui.koltuk_22_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_22_btn))
+        self.ui.koltuk_23_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_23_btn))
+        self.ui.koltuk_24_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_24_btn))
+        self.ui.koltuk_25_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_25_btn))
+        self.ui.koltuk_26_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_26_btn))
+        self.ui.koltuk_27_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_27_btn))
+        self.ui.koltuk_28_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_28_btn))
+        self.ui.koltuk_29_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_29_btn))
+        self.ui.koltuk_30_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_30_btn))
+        self.ui.koltuk_31_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_31_btn))
+        self.ui.koltuk_32_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_32_btn))
+        self.ui.koltuk_33_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_33_btn))
+        self.ui.koltuk_34_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_34_btn))
+        self.ui.koltuk_35_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_35_btn))
+        self.ui.koltuk_36_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_36_btn))
+        self.ui.koltuk_37_btn.clicked.connect(partial(controller.selectSeat, self.ui.koltuk_37_btn))
 
 
 app = QtWidgets.QApplication([])
